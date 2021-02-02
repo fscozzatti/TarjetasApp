f34b903f2de16fea9dcbd4a4a83a33c7d04d29b6
fscozzatti <fscozzatti@gmail.com>
2021-02-02 11:44:05 -0300

    Guardo los ultimos avances de las consultas a firebase para cambiar de rama para probar de instalar una nueva libreria que me permita eliminar el problema de CORS policy

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,63 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import axios from 'axios'
-import firebase from 'firebase/app';
-import 'firebase/analytics'
-import 'firebase/auth'
-import 'firebase/database'
+import axios from 'axios';
 import WelcomePage from './pages/WelcomePage'
 import MainPage from './pages/MainPage'
 import CardPage from './pages/CardPage'
 import NotFoundPage from './pages/NotFoundPage'
 
-
-
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyARWsNdpXlAGW63tSaOKP04ySGR5h1l6O0',
-  authDomain: 'app-tarjetasbd.firebaseapp.com',
-  databaseURL: 'https://app-tarjetasbd.firebaseio.com',
-  projectId: 'app-tarjetasbd',
-  storageBucket: 'app-tarjetasbd.appspot.com',
-  messagingSenderId: '751504794647',
-  appId: '1:751504794647:web:bcc197fb7e3540a4352852',
-  measurementId: 'G-T9X5PBPBE2'
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-
 const App2 = () => {
     
-    const [ todos1, setTodos1 ] = useState([])
+    const [todos1, setTodos1 ] = useState([])
     const [error, setError] = useState(null)
-    useEffect(() => {  
-      async function fetchData() {
-        try {
-            var databaseRef = await firebase.database().ref("/todos");
-            databaseRef.once('value', function(snapshot) {
-            var todos = [];
-            snapshot.forEach(function(childSnapshot) {
-                var data = childSnapshot.val();        
-                todos.push({ title: data.title, descriton: data.descriton, id: data.id, priority: data.priority, responsible: data.responsible}); // key: key,
-            });
-            setTodos1(todos)
-            });
 
-        } catch (error) {
-            if (error.response) { // Errores que nos responde el server
+    useEffect(() => {  
+    async function fetchData() {
+    const res = await axios.get('https://app-tarjetasbd.firebaseio.com/todos.json')
+        .then(resAux => setTodos1(resAux.data))
+        .catch( (err) => {
+            if (err.response) { // Errores que nos responde el server
                 setError("Ha ocurrido un error en el servidor del clima")
-            } else if (error.request) { // Errores que suceden por no llegar al server
+            } else if (err.request) { // Errores que suceden por no llegar al server
                 setError("Verifique la conexión a internet")
             } else { // Errores imprevistos
                 setError("Error al cargar los datos")
-            }                
-        }        
-      }
+                console.log(err)
+            }
+        })  
+    }
   
       fetchData()
     }, []);
+
+    async function putData(id, todo) {
+        const res1 = await axios.put('https://app-tarjetasbd.firebaseio.com/todos.json/' + id, todo,
+         {headers: {
+             'Content-Type': 'application/json',
+             'Access-Control-Allow-Origin' : '*',
+             'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',}})
+        .then(res => { })        
+        .catch( (err) => {
+            if (err.response) { // Errores que nos responde el server
+                setError("Ha ocurrido un error en el servidor del clima")
+                console.log(err)
+            } else if (err.request) { // Errores que suceden por no llegar al server
+                setError("Verifique la conexión a internet")
+                console.log(err)
+            } else { // Errores imprevistos
+                setError("Error al cargar los datos")
+                console.log(err)
+            }
+        }) 
+        console.log('1: ', res1) 
+    }
 
 
     const handleSubmit = ( id, titulo, responsable, descripcion, prioridad ) => {
@@ -73,9 +66,11 @@ const App2 = () => {
         if (index !== -1 ){
             todos1.splice( index, 1, todo)
             setTodos1(todos1) 
-            axios.put('http://localhost:3001/todos/' + id, todo)
+            putData(id,todo)
+            /*axios.put('https://app-tarjetasbd.firebaseio.com/todos.json/' + id, todo)
             .then(res => {
-            })   
+            })*/
+
         }else{
             setTodos1([ ...todos1, todo])  
             axios.post('http://localhost:3001/todos', todo)
